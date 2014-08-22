9cdd9eaee6e24d962a25435f81e7375ccf0fa9ca
SopaXorzTaker <yellowmark@yandex.ru>
2014-08-22 11:21:01 +0400

    fixed init

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ def bind(self, msg):
     if filmsg == "!help":
         self.sendmsg(source, "Hi! I am DogeChat, a IRC waterbowl bot, please donate by"
                              " /msg Doger tip dogechat-bot <amount>")
-    elif source == config['channel'] and random.randint(0, 10) == 5 and not sourcenick in excluded_nicks \
+    elif source in config['channels'] and random.randint(0, 10) == 5 and not sourcenick in excluded_nicks \
             and len(filmsg) > 9:
             dogewon = random.randint(1, 20)
             self.sendmsg(source, "Wow! " + sourcenick + " just won " + str(dogewon) + " doges!")
             self.sendmsg("Doger", "tip " + sourcenick + " " + str(dogewon))
-        
+
 def init(self):
-    self.sendmsg("NickServ", config['pass'])
+    self.sendmsg("NickServ", "identify" + config['pass'])
 client = irc.IRCClient("irc.freenode.net", 6667, config['nick'], config['channels'], bind, init).client
-excluded = open("excluded.txt", "r")
-excluded_nicks = excluded.readlines()
-excluded.close()
+try:
+    excluded = open("excluded.txt", "r")
+    excluded_nicks = excluded.readlines()
+    excluded.close()
+except IOError:
+    print("Error reading excluded.txt!")
+    excluded_nicks = []
